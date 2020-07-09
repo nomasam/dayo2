3366ba42b633576ef19a34b893109daf4208dd15
Nomasam Mbanjwa <nombanjw@c4r21s5.wethinkcode.co.za>
2020-07-09 16:49:23 +0200

    ex04

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb(void)
+{
+	char a = 'a';
+	char b = 'b';
+	char c = 'c';
+//	char comma = ',';
+
+
+		while(a<='9')
+		{
+			while(b<='9')
+			{
+				while(c<='9'){
+
+					if(a<b && b<c){
+						ft_putchar('0');
+						ft_putchar('0');
+						ft_putchar('0');
+
+						ft_putchar(',');
+				    	ft_putchar(' ');
+					}
+					c++;
+				} c='0'; b++;} b='0'; a++;}
+		
+}
+
+	
